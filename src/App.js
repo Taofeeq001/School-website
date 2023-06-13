59f5cb609611401cd5c51d3174b126c35b9104ca
unknown <toshconsultinc@gmail.com>
2023-06-13 14:31:14 +0100

    create an apply page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import Home from './Components/Home/Home';
+import Apply from './Components/Apply/Apply';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
     //   </Routes>
     // </BrowserRouter>
     <div>
-      <Home/>
+      <Apply />
     </div>
   );
 }
